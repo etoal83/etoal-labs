88772734925a0ffb5574e06f7624428ede88eaa5
etoal83 <koki.okutomi@gmail.com>
2020-06-27 00:57:51 +0900

    Add minimal index pages

---
+++ pages/articles/index.js
@@ -0,0 +1,8 @@
+const Articles = () => (
+  <div>
+    <h1>Dr. EtoAl's Lab</h1>
+    <p>This is the index page of my articles.</p>
+  </div>
+)
+
+export default Articles
---
+++ pages/works/index.js
@@ -0,0 +1,8 @@
+const Works = () => (
+  <div>
+    <h1>Dr. EtoAl's Lab</h1>
+    <p>This is the index page of my works.</p>
+  </div>
+)
+
+export default Works
