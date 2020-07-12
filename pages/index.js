bf862671f687470b25acf4697e5ab35812da7c1f
etoal83 <koki.okutomi@gmail.com>
2020-07-13 01:30:37 +0900

    Add 3D pink sphere

--- pages/index.js
+++ pages/index.js
@@ -7,35 +7,47 @@ import { Canvas, useFrame } from 'react-three-fiber'
 const fullWindowStyle = css({
   width: '100vw',
   height: '100vh',
+  backgroundColor: '#222',
 });
 
 const Sample = () => {
   const ref = useRef();
 
   useFrame(() => {
-    ref.current.rotation.z += 0.01;
   });
 
   return (
-    <mesh ref={ref}>
-      <planeBufferGeometry attach='geometry' args={[1, 1]} />
-      <meshBasicMaterial
+    <mesh
+      ref={ref}
+      onClick={e => console.log('click')}
+      onPointerOver={e => console.log('hover')}
+      onPointerOut={e => console.log('unhover')}
+
+    >
+      <sphereGeometry attach='geometry' args={[300, 30, 30]} />
+      <meshStandardMaterial
         attach='material'
         color='hotpink'
-        opacity={0.5}
-        transparent
       />
     </mesh>
   )
 }
 
+const Earth = () => (
+  <Canvas camera={{ position: [0, 0, 1000] }}>
+    <directionalLight
+      position={[500, 500, 500]}
+      intensity={0.8}
+    />
+    <Sample />
+  </Canvas>
+)
+
 const Home = () => (
   <div>
     <Header />
     <div css={fullWindowStyle}>
-      <Canvas>
-        <Sample />
-      </Canvas>
+      <Earth />
     </div>
   </div>
 )
