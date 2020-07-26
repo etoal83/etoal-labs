b584d01181014767b7ae62f6ed502330eefd3a6d
etoal83 <koki.okutomi@gmail.com>
2020-07-26 21:08:46 +0900

    Reconcile emotion-reset with global styles

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,9 @@ import emotionReset from 'emotion-reset'
 
 const globalStyles = css`
   ${emotionReset}
-  * {
-    font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, sans-serif;
+  html {
+    font-family: "Helvetica Neue", Arial, "Hiragino Kaku Gothic ProN", "Hiragino Sans", Meiryo, Roboto, sans-serif;
+    box-sizing: border-box;
   }
 `;
 
