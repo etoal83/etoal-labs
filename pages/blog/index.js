d3ab7cc8e534db32ea7dd1e101ff7ab45c76065b
etoal83 <koki.okutomi@gmail.com>
2020-08-26 23:39:10 +0900

    Display fetched blog posts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,8 +1,45 @@
-const Blogs = () => (
-  <div>
-    <h1>Blogs</h1>
-    <p>This is the index page of my blog posts.</p>
-  </div>
-)
+import { fetchBlogPosts } from '../../lib/contentful/api.js';
+import { richTextFromMarkdown } from '@contentful/rich-text-from-markdown';
 
-export default Blogs
+const BlogPostsPage = ({ posts }) => {
+  return (
+    <>
+      <BlogPosts posts={posts} />
+    </>
+  );
+};
+
+const BlogPosts = ({ posts }) => {
+  console.log(posts);
+
+  return (
+    <div>
+      <h1>Blogs</h1>
+      {posts.map((p) => (
+        <BlogCard title={p.title} body={p.body} />
+      ))}
+    </div>
+  );
+};
+
+const BlogCard = ({ title, body }) => (
+  <div>
+    <h2>{title}</h2>
+    <div>{body}</div>
+  </div>
+);
+
+export const getStaticProps = async () => {
+  const items = await fetchBlogPosts();
+  const posts = await items.map((item) => {
+    return item.fields;
+  });
+
+  return {
+    props: {
+      posts: posts,
+    },
+  };
+};
+
+export default BlogPostsPage;
