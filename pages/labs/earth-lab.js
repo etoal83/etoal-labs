5db450fcd3716a865c5e88433f778142c3bb6296
etoal83 <koki.okutomi@gmail.com>
2020-09-06 02:02:04 +0900

    Set far plane, refactor Earth by using Planet component

--- pages/labs/earth-lab.js
+++ pages/labs/earth-lab.js
@@ -9,22 +9,19 @@ import {
 
 import FullWindowContainer from '../../components/FullWindowContainer';
 import CelestialSphere from '../../components/r3f/CelestialSphere';
+import Planet from '../../components/r3f/Planet';
 
-const Earth = () => {
-  const ref = useRef();
-  const earthTexture = useTextureLoader('/textures/2k_earth_daymap.jpg');
-
-  useFrame(() => {
-    ref.current.rotation.y += 0.001;
-  });
-
-  return (
-    <mesh ref={ref}>
-      <sphereGeometry attach="geometry" args={[300, 64, 64]} />
-      <meshStandardMaterial attach="material" map={earthTexture} />
-    </mesh>
-  );
-};
+const Earth = () => (
+  <Planet
+    physicalProps={{
+      equatorialRadius: 6378.1e3,
+      flattening: 0.003353,
+      siderealRotationPeriod: 23 * 3600 + 56 * 60 + 4.1,
+    }}
+    orbitalProps={{}}
+    texturePath={'/textures/2k_earth_daymap.jpg'}
+  />
+);
 
 const EarthLab = () => (
   <FullWindowContainer>
@@ -32,17 +29,23 @@ const EarthLab = () => (
       style={{ background: '#222' }}
       gl={{ logarithmicDepthBuffer: true }}
     >
-      <PerspectiveCamera makeDefault position={[900, 0, 0]} />
+      <PerspectiveCamera
+        makeDefault
+        position={[3e7, 0, 0]}
+        near={100}
+        far={3.0e16}
+      />
       <OrbitControls
         enableDamping={true}
         dampingFactor={0.15}
-        minDistance={400}
+        minDistance={1e7}
       />
       <directionalLight position={[500, 500, 500]} intensity={0.9} />
       <Suspense fallback={null}>
         <Earth />
         <CelestialSphere />
       </Suspense>
+      {/* <Stats /> */}
     </Canvas>
   </FullWindowContainer>
 );
