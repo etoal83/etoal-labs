11a23ef3b237c6b4d90b978b1d063dd779b6004b
etoal83 <koki.okutomi@gmail.com>
2020-10-08 23:37:47 +0900

    Update empty_shader_2d and shaders

--- pages/gallery/shader/emptyFragmentShader.glsl
+++ pages/gallery/shader/emptyFragmentShader.glsl
@@ -1,7 +1,12 @@
 varying vec2 vUv;
+uniform float uAspect;
 
-void main(){
-    vec4 color=vec4(vUv.x,vUv.y,0.,1.);
+void main() {
+    vec2 uv = vec2(vUv.x * uAspect, vUv.y);
+    vec2 center = vec2(0.5 * uAspect, 0.5);
+    float dist = 0.05 / length(uv - center);
+    vec4 color = vec4(vec3(dist), 1.0);
+    color *= vec4(0.3, 0.3, 1.0, 1.0);
     
-    gl_FragColor=color;
+    gl_FragColor = color;
 }
--- pages/gallery/shader/empty_shader_2d.js
+++ pages/gallery/shader/empty_shader_2d.js
@@ -1,28 +1,39 @@
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useThree } from 'react-three-fiber';
 import { OrthographicCamera } from 'drei';
 import FullWindowContainer from '../../../components/FullWindowContainer';
 
 import emptyVertexShader from './emptyVertexShader.glsl';
 import emptyFragmentShader from './emptyFragmentShader.glsl';
 
-const EmptyShaderCanvas = () => (
-  <FullWindowContainer>
-    <Canvas>
-      <OrthographicCamera args={[-1, 1, 1, -1, 0, 1]} />
-      <mesh>
-        <planeGeometry attach="geometry" args={[2, 2, 10, 10]} />
-        <shaderMaterial
-          attach="material"
-          args={[
-            {
-              vertexShader: emptyVertexShader,
-              fragmentShader: emptyFragmentShader,
+const CustomShaderScreen = () => {
+  const { aspect } = useThree();
+
+  return (
+    <mesh>
+      <planeGeometry attach="geometry" args={[2, 2, 10, 10]} />
+      <shaderMaterial
+        attach="material"
+        args={[
+          {
+            uniforms: {
+              uAspect: { value: aspect },
             },
-          ]}
-        />
-      </mesh>
+            vertexShader: emptyVertexShader,
+            fragmentShader: emptyFragmentShader,
+          },
+        ]}
+      />
+    </mesh>
+  );
+};
+
+const EmptyShaderCanvas = () => {
+  return (
+    <Canvas style={{ height: '100vh', width: '100vw' }}>
+      <OrthographicCamera args={[-1, 1, 1, -1, 0, 1]} />
+      <CustomShaderScreen />
     </Canvas>
-  </FullWindowContainer>
-);
+  );
+};
 
 export default EmptyShaderCanvas;
