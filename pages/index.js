ccedde818ec6b7c898e1101afcddfb81c8c2cca2
etoal83 <koki.okutomi@gmail.com>
2020-10-26 23:47:13 +0900

    Rewrite home page with View component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,20 @@
 import Header from '../components/Header';
 import EarthLab from './labs/earth-lab';
-import { jsx } from '@emotion/core';
+import { StyleSheet, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 const Home = () => (
-  <>
+  <View style={styles.container}>
     <Header />
     <EarthLab />
-  </>
+  </View>
 );
 
 export default Home;
