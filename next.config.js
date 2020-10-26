35e0fa31a98161d6e5324af3856d8a1929894b9d
etoal83 <koki.okutomi@gmail.com>
2020-10-26 23:44:02 +0900

    yarn add -D @expo/next-adapter

--- next.config.js
+++ next.config.js
@@ -1,17 +1,32 @@
+const { withExpo } = require('@expo/next-adapter');
 const withTM = require('next-transpile-modules')([
   'drei',
   'three',
   'postprocessing',
 ]);
 
-module.exports = withTM({
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(glsl|vs|fs|vert|frag)$/,
-      exclude: /node_modules/,
-      use: ['raw-loader', 'glslify-loader'],
-    });
+module.exports = withExpo(
+  withTM({
+    projectRoot: __dirname,
+    webpack: (config, options) => {
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        // Transform all direct `react-native` imports to `react-native-web`
+        'react-native$': 'react-native-web',
+      };
+      config.resolve.extensions = [
+        '.web.js',
+        '.web.ts',
+        '.web.tsx',
+        ...config.resolve.extensions,
+      ];
+      config.module.rules.push({
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ['raw-loader', 'glslify-loader'],
+      });
 
-    return config;
-  },
-});
+      return config;
+    },
+  })
+);
